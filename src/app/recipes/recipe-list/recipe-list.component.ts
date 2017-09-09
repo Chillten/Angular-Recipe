d2221917890fc326c5f41a1b48c923f34c7e2a09
chillten <bogovich.alexander@gmail.com>
2017-09-09 15:24:40 +0700

    Add tslint module with aribnb ext

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Recipe} from '../../model/recipe.model';
+import { Recipe } from '../../model/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,10 +9,18 @@ import {Recipe} from '../../model/recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is simply a test', 'http://www.publicdomainpictures.net/pictures/10000/velka/1210-1240955754dalH.jpg'),
-    new Recipe('A test recipe', 'This is simply a test', 'http://www.publicdomainpictures.net/pictures/10000/velka/1210-1240955754dalH.jpg'),
-    new Recipe('A test recipe', 'This is simply a test', 'http://www.publicdomainpictures.net/pictures/10000/velka/1210-1240955754dalH.jpg'),
-    new Recipe('A test recipe', 'This is simply a test', 'http://www.publicdomainpictures.net/pictures/10000/velka/1210-1240955754dalH.jpg')
+    new Recipe('A test recipe1',
+               'This is simply a test1',
+               'http://www.publicdomainpictures.net/pictures/10000/velka/1210-1240955754dalH.jpg'),
+    new Recipe('A test recipe2',
+               'This is simply a test2',
+               'http://www.publicdomainpictures.net/pictures/10000/velka/1210-1240955754dalH.jpg'),
+    new Recipe('A test recipe3',
+               'This is simply a test3',
+               'http://www.publicdomainpictures.net/pictures/10000/velka/1210-1240955754dalH.jpg'),
+    new Recipe('A test recipe4',
+               'This is simply a test4',
+               'http://www.publicdomainpictures.net/pictures/10000/velka/1210-1240955754dalH.jpg')
   ];
 
   constructor() { }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import {Ingredient} from "../common/model/ingredient.model";
+import { Ingredient } from '../common/model/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients:Ingredient[] = [
+  ingredients: Ingredient[] = [
     new Ingredient('Apple', 5),
     new Ingredient('Orange', 10)
   ];
