bf3ee2841058242326e1da049c330a111503e89f
chillten <bogovich.alexander@gmail.com>
2017-09-10 16:13:00 +0700

    Add ingredient to recipe model & output on recipe detail
    Signed-off-by: chillten <bogovich.alexander@gmail.com>

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from '../model/recipe.model';
+import { Ingredient } from '../common/model/ingredient.model';
 
 @Injectable()
 export class RecipeService {
@@ -7,18 +8,21 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
-    new Recipe('A test recipe1',
-               'This is simply a test1',
-               'http://www.publicdomainpictures.net/pictures/10000/velka/1210-1240955754dalH.jpg'),
-    new Recipe('A test recipe2',
-               'This is simply a test2',
-               'http://www.publicdomainpictures.net/pictures/10000/velka/1210-1240955754dalH.jpg'),
-    new Recipe('A test recipe3',
-               'This is simply a test3',
-               'http://www.publicdomainpictures.net/pictures/10000/velka/1210-1240955754dalH.jpg'),
-    new Recipe('A test recipe4',
-               'This is simply a test4',
-               'http://www.publicdomainpictures.net/pictures/10000/velka/1210-1240955754dalH.jpg')
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe(
+      'Big Fat Burger',
+      'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
   ];
 
   constructor() { }
