eaca0b09e409aa1cbdb2e377183b763c48e46dfb
chillten <bogovich.alexander@gmail.com>
2017-10-15 16:10:09 +0700

    Use HttpParams

--- src/app/shared/dao.service.ts
+++ src/app/shared/dao.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { RecipeService } from '../recipes/recipe.service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { AuthService } from '../auth/auth.service';
 import 'rxjs/Rx';
 import { fromPromise } from 'rxjs/observable/fromPromise';
@@ -9,19 +9,29 @@ import { Recipe } from './model/recipe.model';
 @Injectable()
 export class DaoService {
 
+  private firebaseUrl = 'https://bogovich-angular-recipe.firebaseio.com/';
+  private recipesUrl = this.firebaseUrl.concat('recipes.json');
+
   constructor(private recipeService: RecipeService, private httpClient: HttpClient, private authService: AuthService) { }
 
   storeRecipe() {
     fromPromise(this.authService.getToken())
-      .map(token => 'https://bogovich-angular-recipe.firebaseio.com/recipes.json?auth='.concat(token))
-      .flatMap(url => this.httpClient.put(url, this.recipeService.getRecipes()))
+      .flatMap(token =>
+        this.httpClient.put(
+          this.recipesUrl,
+          this.recipeService.getRecipes(),
+          { params: new HttpParams().set('auth', token) })
+      )
       .subscribe();
   }
 
   loadRecipes() {
     fromPromise(this.authService.getToken())
-      .map(token => 'https://bogovich-angular-recipe.firebaseio.com/recipes.json?auth='.concat(token))
-      .flatMap(url => this.httpClient.get<Recipe[]>(url))
+      .flatMap(token =>
+        this.httpClient.get<Recipe[]>(
+          this.recipesUrl,
+          { params: new HttpParams().set('auth', token) })
+      )
       .map(Recipe.validateRecipes)
       .subscribe(r => this.recipeService.setRecipes(r));
   }
